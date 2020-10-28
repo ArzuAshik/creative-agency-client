279e53f6e037a492c349ae1fe5e08323560401e5
ArzuAshik <arzu2050@gmail.com>
2020-10-28 23:32:07 +0600

    documentation update

--- src/Components/HomePage/Services/Services.js
+++ src/Components/HomePage/Services/Services.js
@@ -12,13 +12,10 @@ const Services = ({ service }) => {
   });
 
   return (
-    <div
-      onMouseOver={() => toggle(!state)}
-      className="service-item col-md-4 text-center p-5"
-    >
+    <div className="col-md-4" onMouseOver={() => toggle(!state)}>
       <animated.div
         style={{
-          opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+          opacity: x.interpolate({ range: [0, 1], output: [1, 1] }),
           transform: x
             .interpolate({
               range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
@@ -27,17 +24,19 @@ const Services = ({ service }) => {
             .interpolate((x) => `scale(${x})`),
         }}
       >
-        <Link to={`/dashboard/order/${service._id}`}>
-          <img
-            className="d-inline-block rounded-circle"
-            height="74"
-            width="74"
-            src={`data:image/jpeg;base64,${service.img.img}`}
-            alt={service.title}
-          />
-          <h5 className="my-2">{service.title}</h5>
-          <p className="text-secondary">{service.description}</p>
-        </Link>
+        <div className="service-item text-center p-5">
+          <Link to={`/dashboard/order/${service._id}`}>
+            <img
+              className="d-inline-block rounded-circle"
+              height="74"
+              width="74"
+              src={`data:image/jpeg;base64,${service.img.img}`}
+              alt={service.title}
+            />
+            <h5 className="my-2">{service.title}</h5>
+            <p className="text-secondary">{service.description}</p>
+          </Link>
+        </div>
       </animated.div>
     </div>
   );
